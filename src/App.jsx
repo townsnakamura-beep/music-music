9c5f77cbc8bca67d8631b607cb4ca43619bfd72f
townsnakamura-beep <town.s.nakamura@gmail.com>
2026-08-15 10:07:33 +0900

    fix: TURNサーバー追加でNAT越え対応

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,25 @@ import './App.css'
 const SIGNALING_SERVER_URL = 'https://music-music.onrender.com'
 
 const ICE_SERVERS = {
-  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:19302' },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turns:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+  ],
 }
 
 function App() {
