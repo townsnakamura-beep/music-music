b2e2169e0232955e27452c11e958bc28adeaaeab
townsnakamura-beep <town.s.nakamura@gmail.com>
2026-08-27 22:33:56 +0900

    Add ASIO audio output methods to preload
    Added new methods for ASIO audio output control.

--- preload.cjs
+++ preload.cjs
@@ -1,7 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  // 既存
   startAudio: () => ipcRenderer.invoke('start-audio'),
   stopAudio: () => ipcRenderer.invoke('stop-audio'),
   onAudioData: (callback) => ipcRenderer.on('audio-data', (event, data) => callback(data)),
+
+  // 新規追加：ASIO出力
+  startAudioOutput: () => ipcRenderer.invoke('start-audio-output'),
+  stopAudioOutput: () => ipcRenderer.invoke('stop-audio-output'),
+  audioPlay: (pcmBuffer) => ipcRenderer.send('audio-play', pcmBuffer),
 })
